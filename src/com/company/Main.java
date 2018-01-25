763702893cda63c428f0d0332d31fa5b55e6476c
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-01-25 15:00:54 -0500

    Finished project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	/* && = and
+
+	 */
+    int a = 2;
+    int x = 6;
+    int y =1;
+    int b =4;
+
+    if (y +y == a || a == x){
+        System.out.println("y+y == a || a == x");}
+
+        if (a != x && y+y == a){
+        System.out.println("!(a == x) && y + y == a");}
+        if (a<b){
+        System.out.println("a<b");
+        }
+
+
+
+
+
+
     }
 }
